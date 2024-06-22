160057f7882d19b7507b53425db8f31779fe6727
MohamedSaad00 <mohamed.saadx4@gmail.com>
2024-06-21 19:17:54 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <ctype.h>
+
 /**
-*main - Program to print alphabet letter followed by new line
-*
-*Return: return 0
-*/
+ * main - Program to print alphabet letters followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int letter;
-	
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		letter = tolower(letter);
-		putchar(letter);
-	}
-	putchar('\n');
-	return (0);
+    int letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        letter = tolower(letter);
+        putchar(letter);
+    }
+    putchar('\n');
+    return (0);
 }
