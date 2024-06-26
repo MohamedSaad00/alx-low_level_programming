92a7232cb76cb3d729bbbe8c0110e26d8ab33943
MohamedSaad00 <mohamed.saadx4@gmail.com>
2024-06-25 20:08:57 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+*main - prints out first 50
+*fibonacci suit numbers
+*Return: return 0
+*/
+
+int main(void)
+{
+    long int a = 1, b = 2, next;
+    int i;
+
+    printf("%ld, %ld", a, b);
+
+    for (i = 2; i < 50; i++)
+    {
+        next = a + b;
+        printf(", %ld", next);
+        a = b;
+        b = next;
+    }
+    
+    printf("\n");
+    return (0);
+}
