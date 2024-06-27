3643d4a83de3939c2d5044e87b1eab9d32115e4f
MohamedSaad00 <mohamed.saadx4@gmail.com>
2024-06-27 08:41:42 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * _putchar - that prints the numbers, from 0 to 9, followed by a new line.
- *
- * No Return (void)
- */
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+
 void print_numbers(void)
 {
 int num = 48;
