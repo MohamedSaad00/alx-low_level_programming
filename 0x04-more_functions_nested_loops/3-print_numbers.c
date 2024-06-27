09840561869059138fa3ade020f436b6081840ef
MohamedSaad00 <mohamed.saadx4@gmail.com>
2024-06-27 08:40:47 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _putchar - that prints the numbers, from 0 to 9, followed by a new line.
+ *
+ * No Return (void)
+ */
+void print_numbers(void)
+{
+int num = 48;
+for (num = 48; num < 58; num++)
+{
+_putchar(num);
+}
+_putchar(10);
+}
