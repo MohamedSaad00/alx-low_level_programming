5349f56696cf43191938fff6c4e1c43cad25551b
MohamedSaad00 <mohamed.saadx4@gmail.com>
2024-07-04 18:42:35 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,29 +4,29 @@
 *@str: string to capitalize
 *Return:returns the capitalized string
 */
+
 char *cap_string(char *str)
 {
-	int index = 0;
+    int index = 0;
 
-	while (str[++index])
+    if (str[index] >= 'a' && str[index] <= 'z')
+    {
+        str[index] -= 32;
+    }
+    while (str[index] != '\0')
+    {
+        if (str[index] == ' ' || str[index] == '\t' || str[index] == '\n' ||
+            str[index] == ',' || str[index] == ';' || str[index] == '.' ||
+            str[index] == '!' || str[index] == '?' || str[index] == '"' ||
+            str[index] == '(' || str[index] == ')' || str[index] == '{' ||
+            str[index] == '}')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
-			str[index] -= 32;
+	if (str[index + 1] >= 'a' && str[index + 1] <= 'z')
+	{
+	str[index + 1] -= 32;
+	}
+	}
+	index++;
 	}
 	return (str);
 }
