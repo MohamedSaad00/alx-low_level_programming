5be00325e54d957514dde57221a47d6093f5a252
MohamedSaad00 <mohamed.saadx4@gmail.com>
2024-07-04 17:39:46 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
 *cap_string - function that capitalize first character of a word
 *@str: string to capitalize
 *Return:returns the capitalized string
 */
-
 char *cap_string(char *str)
 {
 	int index = 0;
+
 	while (str[++index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
-		if (str[index - 1] == ' ' || 
+		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
 				str[index - 1] == ',' ||
@@ -23,8 +22,8 @@ char *cap_string(char *str)
 				str[index - 1] == '!' ||
 				str[index - 1] == '?' ||
 				str[index - 1] == '"' ||
-				str[index - 1] == ')' ||
 				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
 				str[index - 1] == '{' ||
 				str[index - 1] == '}')
 			str[index] -= 32;
