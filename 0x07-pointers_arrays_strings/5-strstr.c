e049e5fcc821880e56bf3a56e9d7b1d1db3e6dc7
MohamedSaad00 <mohamed.saadx4@gmail.com>
2024-07-08 14:55:25 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,20 +13,20 @@
 char *_strstr(char *haystack, char *needle)
 {
 	char *h, *n;
-	if(*needle == '\0')
-		return haystack;
-	while (*haystack)
+if(*needle == '\0')
+	return haystack;
+while (*haystack)
 	{
-		h = haystack;
-		n = needle;
-		while(*h && *n && (*h == *n))
-		{
-			h++;
-			n++;
-		}
+	h = haystack;
+	n = needle;
+	while(*h && *n && (*h == *n))
+	{
+		h++;
+		n++;
+	}
 		if (*n == '\0')
-			return haystack;
-		haystack++;
+		return haystack;
+	haystack++;
 	}
 	return NULL;
 }
