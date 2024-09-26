de12bb3d5eb2dc3ecbe01270f14ebdc224ac72e0
MohamedSaad00 <mohamed.saadx4@gmail.com>
2024-09-26 13:43:54 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-export LD_PRELOAD=./win_giga_millions.so
+wget -P /tmp/ https://raw.github.com/Pericles001/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD =/tmp/nrandom.so
